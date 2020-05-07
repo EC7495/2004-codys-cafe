489c8fab2a6bf195557967f73841d1c2b182058b
Filadelfo Braz <filadelfo.braz@gmail.com>
2020-05-07 16:11:35 -0400

    get request for api/coffee

--- server/routes/coffee.router.js
+++ server/routes/coffee.router.js
@@ -5,4 +5,45 @@ const {Coffee} = require('../models')
 // Remember that these routes are already mounted on
 // /api/coffee!
 
+router.get("/", async (req, res, next) => {
+  try {
+    const coffees = await Coffee.findAll()
+    res.send(coffees)
+  } catch (err) {
+    next(e)
+  }
+})
+
+router.get("/ingredients/:ingredientName", async (req, res, next) => {
+  try {
+    const ingredientName = req.params.ingredientName
+    const coffeeList = await Coffee.findByIngredient(ingredientName)
+
+    res.json(coffeeList)
+
+  } catch (err) {
+    next(err)
+  }
+})
+
+router.get("/:coffeeId", async (req, res, next) => {
+  try {
+
+    const coffeeId = req.params.coffeeId
+
+    const data = await Coffee.findById(coffeeId)
+
+    if(!data) {
+      throw new Error("Not found")
+      //res.sendStatus(404)
+    } else {
+      res.send(data)
+    }
+
+  } catch (err) {
+    err.status = 404
+    next(err)
+  }
+})
+
 module.exports = router
