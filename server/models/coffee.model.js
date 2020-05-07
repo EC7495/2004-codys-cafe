942fe8c7e3767cf6aeb33245dad0e296d8ac986f
Erick <ecanals07@gmail.com>
2020-05-07 15:13:16 -0400

    defined coffee model

--- server/models/coffee.model.js
+++ server/models/coffee.model.js
@@ -1,8 +1,35 @@
-const Sequelize = require('sequelize')
-const db = require('./database')
+const Sequelize = require('sequelize');
+const db = require('./database');
+const { Op } = require('sequelize');
 
 const Coffee = db.define('coffee', {
   // your code here
-})
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
 
-module.exports = Coffee
+  ingredients: {
+    type: Sequelize.ARRAY(Sequelize.STRING),
+  },
+});
+
+Coffee.prototype.getIngredients = function () {
+  return this.ingredients.join(', ');
+};
+
+Coffee.findByIngredient = async function (ingredient) {
+  return await Coffee.findAll({
+    where: {
+      ingredients: {
+        [Op.contains]: [ingredient],
+      },
+    },
+  });
+};
+
+Coffee.beforeValidate(element => {
+  if (!element.ingredients.includes('love')) element.ingredients.push('love');
+});
+
+module.exports = Coffee;
